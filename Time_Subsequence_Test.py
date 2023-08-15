accbb6440b4cb5671717478a725d2fc73fffe4aa
fefenfeng <1369508762@qq.com>
2023-08-15 14:44:11 +0100

    Time_subsequence_test done

--- Time_Subsequence_Test.py
+++ Time_Subsequence_Test.py
@@ -1,31 +1,42 @@
 import torch
 
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 from Model.InceptionTime import InceptionTime
 from ReadData import load_data_with_index, MyDataset
 
 inceptiontime = InceptionTime()
-state_path = "./State_dict/Trial0_subsequence_test/Prosaccades_gap/Fold_3.pth"
+state_path = "./State_dict/Trial1_subsequence_test/Stepped_random_motion_1/Fold_5.pth"
 inceptiontime.load_state_dict(torch.load((state_path)))
 
-accuracy_list = []
-for i in range(5):
-    testdata = load_data_with_index(r"D:\MyFiles\UOB_Robotics22\Dissertation\data_info\SubSequence\Trial0\Prosaccades_gap", [0, 800])
-    test_dataset = MyDataset(testdata)
-    batch_size = 16
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
-    test_dataset_len = len(test_dataset)
-    print("The length of test dataset is :{}".format(test_dataset_len))
-    total_test_accuracy = 0
-    for data in test_loader:
-        positions, targets = data
-        outputs = inceptiontime(positions)
-        accuracy = (outputs.argmax(1) == targets).sum()
-        total_test_accuracy = total_test_accuracy + accuracy
+writer = SummaryWriter("./Logs_tensorboard/Time_Subsequence_Test/Trial1/Stepped_random_motion")
 
-    print("Total test accuracy: {}".format(total_test_accuracy/test_dataset_len))
-    accuracy_list.append((total_test_accuracy/test_dataset_len))
+for i in range(1, 16):
+    index = [0, 160 * i]
 
-Mean_accuracy = sum(accuracy_list) / len(accuracy_list)
-print("Mean test accuracy:{}".format(Mean_accuracy))
+    accuracy_list = []
+    for _ in range(5):
+        testdata = load_data_with_index(r"D:\MyFiles\UOB_Robotics22\Dissertation\data_info\SubSequence\Trial1\Stepped_random_motion_1", index)
+        test_dataset = MyDataset(testdata)
+        batch_size = 16
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+        test_dataset_len = len(test_dataset)
+        # print("The length of test dataset is :{}".format(test_dataset_len))
+        total_test_accuracy = 0
+        for data in test_loader:
+            positions, targets = data
+            outputs = inceptiontime(positions)
+            accuracy = (outputs.argmax(1) == targets).sum()
+            total_test_accuracy = total_test_accuracy + accuracy
+
+        # print("Total test accuracy: {}".format(total_test_accuracy/test_dataset_len))
+        accuracy_list.append((total_test_accuracy/test_dataset_len))
+
+    Mean_accuracy = sum(accuracy_list) / len(accuracy_list)
+    # print("Mean test accuracy:{}".format(Mean_accuracy))
+    print("Mean test accuracy for index {}: {}".format(index, Mean_accuracy))
+
+    writer.add_scalar('Mean_Accuracy', Mean_accuracy, i)
+
+writer.close()
