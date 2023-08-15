5ac67121292a55ef100a4ec9f211b48ba18eb481
fefenfeng <1369508762@qq.com>
2023-08-15 01:51:31 +0100

    compute mean and standard deviation

--- Dataprocess/Calculate_Mean_Std.py
+++ Dataprocess/Calculate_Mean_Std.py
@@ -6,9 +6,7 @@ def compute_mean_std(numbers):
     return mean, std_dev
 
 
-numbers = [0.8667, 1, 1, 0.9655, 1,
-           0.9, 0.9333, 0.9655, 0.8966, 0.931,
-           0.8667, 0.9667, 0.931, 0.8966, 1]
+numbers = []
 
 mean, std_dev = compute_mean_std(numbers)
 print(f"Mean: {mean}, Standard Deviation: {std_dev}")
