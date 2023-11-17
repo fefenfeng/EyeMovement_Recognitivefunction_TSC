6938e051ead236fc0520834fe1c16f28152968b8
fefenfeng <1369508762@qq.com>
2023-11-17 16:44:34 +0800

    Simple script to calculate mean and standard deviation of a list of numbers

--- Dataprocess/Calculate_Mean_Std.py
+++ Dataprocess/Calculate_Mean_Std.py
@@ -1,19 +1,15 @@
 import statistics
 
-# Simple script to calculate mean and standard deviation value of a list
-# note! list should not be empty
-
 
 def compute_mean_std(numbers):
     """
-    Simple script to calculate mean and standard deviation value of a list
+    Simple script to calculate mean and standard deviation value of a list.
 
     Args:
-    - numbers (list): A non-empty list of values.
+        numbers: (list) A non-empty list of values.
 
     Returns:
-    - mean: mean value of the list
-    - std_dev: standard deviation of the list
+        A tuple containing two float values, mean and std value.
     """
     mean = sum(numbers) / len(numbers)
     std_dev = statistics.stdev(numbers)
