1629cc722c9b6566841673ea88890528ca0ee4c0
fefenfeng <1369508762@qq.com>
2023-11-20 17:17:11 +0800

    Resort the data by time and delete unused column

--- Dataprocess/Sortbytime.py
+++ Dataprocess/Sortbytime.py
@@ -1,42 +1,41 @@
 import os
 import pandas as pd
 
+"""
+Resort the data by time and delete unused column.
+"""
 
-# resort the data by time and delete unused column
+
 def process_files(source_dir, target_dir):
-    # 获取源文件夹中所有的csv文件
+    # Get all csv files in source directory
     csv_files = [f for f in os.listdir(source_dir) if f.endswith('.csv')]
 
     for file in csv_files:
-        # 读取csv文件
         df = pd.read_csv(os.path.join(source_dir, file))
 
-        # # 从filename列中提取数字
+        # # get time tag from the 'filename' column
         # df['sequence'] = df['filename'].str.extract('-(\d+)\.jpg')[0]
         #
-        # # 检查是否存在NaN值
+        # # check if NaN exists
         # if df['sequence'].isna().any():
         #     print(f"NaN value found in file: {file}")
-        #     df['sequence'] = df['sequence'].fillna('some_value')  # 用适当的值替换'some_value'
+        #     df['sequence'] = df['sequence'].fillna('some_value')  # replace 'some_value'
         #
-        # # 将sequence列转换为整数类型
+        # # transform 'sequence' column into int type
         # df['sequence'] = df['sequence'].astype(int)
-        # # 从filename列中提取数字并转换为整数类型
-        # df['sequence'] = df['filename'].str.extract('-(\d+)\.jpg')[0].astype(int)
         #
-        # # 根据序列号重新排序
+        # # resort by 'sequence' column
         # df = df.sort_values('sequence')
 
-        # # 删除filename、Unnamed: 0列和新添加的sequence列
+        # # drop 'filename', 'Unnamed: 0', 'sequence' columns
         # df = df.drop(['Unnamed: 0', 'filename', 'sequence'], axis=1)
 
         df = df.drop(['Unnamed: 0', 'filename', 'blink'], axis=1)
 
-        # 将处理后的数据保存到目标文件夹
+        # save the data into target directory
         df.to_csv(os.path.join(target_dir, file), index=False)
 
 
-# 调用函数，传入源文件夹路径和目标文件夹路径
 source_dir = r"D:\MyFiles\UOB_Robotics22\Dissertation\data_info\left_eye\trial1\0"
 target_dir = r"D:\MyFiles\UOB_Robotics22\Dissertation\data_info\left_eye\trial1_sorted\0"
 process_files(source_dir, target_dir)
